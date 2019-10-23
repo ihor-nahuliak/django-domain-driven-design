bbe07b4d5ed3cb6932e9e0401138d6bdb48fda53
ihor-nahuliak <ihor.nahuliak@gmail.com>
2019-10-24 00:27:06 +0200

    core: entity types docs

--- app/core/types/entity.py
+++ app/core/types/entity.py
@@ -5,10 +5,23 @@ from app.core.types.bunch import bunch
 
 @dataclasses.dataclass
 class Entity:
-    pass
+    """Base entity class."""
 
 
 class ParaEntity(bunch):
+    """
+    Partial entity like class.
+    Example of usage:
+
+    >>> item = ParaEntity(id=1, username='test')
+    >>> item.id
+    1
+    >>> item.username
+    'test'
+    >>> item.first_name is dataclasses.MISSING
+    True
+
+    """
 
     @classmethod
     def __missing__(cls, name):
