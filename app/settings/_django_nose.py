2cce8e658a0c001e4a133fe74e8c299edf7f876d
ihor-nahuliak <ihor.nahuliak@gmail.com>
2019-11-28 18:32:32 +0100

    Infrastructure: python 3.8 support & tox

--- app/settings/_django_nose.py
+++ app/settings/_django_nose.py
@@ -1,3 +1,5 @@
+import sys
+
 from ._settings import REQUIREMENTS_APPS
 
 
@@ -5,11 +7,18 @@ REQUIREMENTS_APPS.append('django_nose')
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
+
+if sys.version_info[:2] == (3, 8):
+    TEST_TIMEOUT_MS = 1000  # Python 3.8 makes the code too slow...
+else:
+    TEST_TIMEOUT_MS = 300
+
+
 NOSE_ARGS = [
     '--with-doctest',
     '--doctest-tests',
     '--with-timer',
-    '--timer-warning=300ms',
+    f'--timer-warning={TEST_TIMEOUT_MS}ms',
     '--timer-fail=error',
     '--cover-erase',
     '--with-coverage',
