f6b039939bd10168e7e67566823753c2ace7756a
ihor-nahuliak <ihor.nahuliak@gmail.com>
2020-01-20 19:13:53 +0100

    refactoring: choice enum (inline comments doc seems to be too tricky)
    refactoring: choice enum (inline comments doc seems to be too tricky)

--- app/core/types/choice_enum.py
+++ app/core/types/choice_enum.py
@@ -1,16 +1,7 @@
-import inspect
 from enum import Enum, EnumMeta
-
-from django.utils.functional import cached_property
 
 
 class ChoiceEnumMeta(EnumMeta):
-
-    def __new__(metacls, cls, bases, classdict):  # noqa: N804
-        # pylint: disable=bad-mcs-classmethod-argument
-        if '__doc__' not in classdict:
-            classdict['__doc__'] = cached_property(metacls.__value_doc__)
-        return super().__new__(metacls, cls, bases, classdict)
 
     def items(cls):  # noqa: N805
         # pylint: disable=no-value-for-parameter
@@ -36,32 +27,6 @@ class ChoiceEnumMeta(EnumMeta):
             choices=str(cls.items())[1:-1],
         )
 
-    def __value_doc__(cls):  # noqa: N805
-        """
-        Inspects your ChoiceEnum class
-        taking inline comment near the enum value definition.
-
-        :rtype: str | None
-        :return: inline comment
-        """
-        # pylint: disable=no-value-for-parameter
-        doc = None
-
-        try:
-            lines_list, _ = inspect.getsourcelines(cls.__objclass__)
-        except OSError:
-            module = inspect.getmodule(cls.__objclass__)
-            lines_list, _ = inspect.getsourcelines(module)
-
-        value_def = ' {} = {}'.format(cls.name, cls.value)
-        for line in lines_list:
-            if value_def in line and '#' in line:
-                _, inline_comment = line.split('#')
-                doc = inspect.cleandoc(inline_comment)
-                break
-
-        return doc
-
 
 class ChoiceEnum(Enum, metaclass=ChoiceEnumMeta):
     """
@@ -76,8 +41,8 @@ class ChoiceEnum(Enum, metaclass=ChoiceEnumMeta):
     Example of usage:
     >>> class EmailType(ChoiceEnum):
     ...     other = 0
-    ...     personal = 1    # for home
-    ...     corporate = 2   # for work
+    ...     personal = 1
+    ...     corporate = 2
     ...
     >>> EmailType
     EmailType(('other', 0), ('personal', 1), ('corporate', 2))
@@ -90,7 +55,7 @@ class ChoiceEnum(Enum, metaclass=ChoiceEnumMeta):
 
     Has "choices" class property:
     >>> EmailType.choices
-    ((0, 'other'), (1, 'personal (for home)'), (2, 'corporate (for work)'))
+    ((0, 'other'), (1, 'personal'), (2, 'corporate'))
 
     Multi type representation:
     >>> int(EmailType.personal)
@@ -112,17 +77,9 @@ class ChoiceEnum(Enum, metaclass=ChoiceEnumMeta):
     >>> EmailType.personal == EmailType.corporate
     False
 
-    Takes inline comments as value docs:
-    >>> EmailType.personal.__doc__
-    'for home'
-    >>> EmailType.corporate.__doc__
-    'for work'
-
     """
 
     def __repr__(self):
-        if self.__doc__:
-            return f'{self.name} ({self.__doc__})'
         return self.name
 
     def __str__(self):
